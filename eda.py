97c7de7e66ba2535ffdaab05441a7fc569c877b1
Rudy Rivera <rrivera7@uwo.ca>
2024-11-24 17:05:32 -0500

    heat map now under eda.py

---
+++ eda.py
@@ -0,0 +1,67 @@
+# Using Exploratory Data Analysis to understand the data
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+class EDA:
+    def __init__(self):
+        pass
+
+    # Load the dataset
+    def load_data(self):
+        df = pd.read_csv('american_bankruptcy.csv')
+        return df
+    
+    # Create a heatmap of the correlation matrix
+    def heatmap(self, df):
+
+        # Select only numeric columns
+        numeric_df = df.select_dtypes(include=[np.number])
+
+        # Rename the columns
+        numeric_df.columns = ["Year",
+                      "Current Assets", 
+                      "Cost of Goods Sold", 
+                      "Depreciation", 
+                      "EBITDA", 
+                      "Inventory", 
+                      "Net Income", 
+                      "Total Recievables",
+                      "Market Value", 
+                      "Net Sales", 
+                      "Total Assets", 
+                      "Total Long Term Debt",
+                      "Earnings Before Interest & Taxes", 
+                      "Gross Profit", 
+                      "Total Current Liabilities", 
+                      "Retained Earnings", 
+                      "Total Revenue", 
+                      "Total Liabilities", 
+                      "Total Operating Expenses"]
+        
+        cols = numeric_df.columns
+        cm = np.corrcoef(numeric_df[cols].values.T)
+        sns.set(font_scale=1.5)
+        hm = sns.heatmap(cm,
+                         cbar=True,
+                         annot=True,
+                         square=True,
+                         fmt='.2f',
+                         annot_kws={'size': 10},
+                         yticklabels=cols,
+                         xticklabels=cols)
+        plt.tight_layout()
+        plt.show()
+
+    # Main function
+    def main(self):
+        df = self.load_data()
+        self.heatmap(df)
+
+if __name__ == "__main__":
+    EDA().main()
